be78750076cb4b867c83b6302c547adb19843134
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-02-11 22:44:07 -0600

    Added objects and revamped code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,101 @@
 
 function startGame(){
-    var playerName = window.prompt("Name your gladiator!", "");
-    console.log(playerName);
-    var attackerNames = ["Roberto", "Bender", "C3-P0"];
-    var playerHealth = randomNumber(70, 110);
-    var playerAttack = randomNumber(8, 13);
-    var playerMoney = 10;
-    var playerAlive = true;
-    var shopChoice = 1;
-    var attackerHealth = randomNumber(40, 70);
-    var attackerAttack = randomNumber(10, 15);
-    //commences fight
-    for(var i=0; i<attackerNames.length; i++){
-        if(!playerAlive) break;
-        shopChoice = 1;
-        if(i>0){
-            while(shopChoice != "3"){
-                shopChoice = window.prompt("Welcome to the shop, You can 1.Refill: 10 gold, 2.Upgrade: 20 gold, 3.Leave\n\nYour gold: "+ playerMoney +"\n\nEnter 1, 2, or 3:");
-                while(shopChoice != 1 && shopChoice != 2 && shopChoice != 3) shopChoice = window.prompt("ERROR: Invalid selection, your selection, " + shopChoice + ". Acceptable selections are 1, 2, 3.\n\nWelcome to the shop, You can 1.Refill: 10 gold, 2.Upgrade: 20 gold, 3.Leave\n\nYour gold: "+ playerMoney +"\n\nEnter 1, 2, or 3:");
-                switch(shopChoice){
-                    case "1":     if(playerMoney <10){
-                                    window.alert("You don't have enough money for this. Continuing to the next match.\n\nGood luck, Pit Dog");
-                                    shopChoice = "3";
-                                    break;
-                                }
-                                if((playerHealth += 30) > 100) playerHealth = 100;
-                                else playerHealth +=30;
-                                playerMoney -= 10;
-                                break;
-                    
-                    case "2":     
-                                if(playerMoney <20){
-                                    if(playerMoney <10){
-                                        window.alert("You don't have enough money for this. Continuing to the next match.\n\nGood luck, Pit Dog");
-                                        shopChoice = "3";
-                                        break;
-                                    }
-                                    window.alert("You don't have enough money for this.\n\nGood luck, Pit Dog");
-                                    break;
-
-                                }
-                                playerAttack += 3;
-                                playerMoney -= 20;
-                    
-                    default:     break;
-                }
+    var repeat = true;
+    var player = {
+        name: window.prompt("Name your gladiator!"),
+        health: randomNumber(70, 110),
+        attack: randomNumber(8, 13),
+        money: 10,
+        alive: true,
+        reset: function(){
+            this.health = randomNumber(70, 110);
+            this.attack = randomNumber(8, 13);
+            this.money = 10;
+            this.alive = true;
+        },
+        refillHealth: function(){
+            if(this.money <10){
+                window.alert("You don't have enough money for this. Continuing to the next match.\n\nGood luck, Pit Dog");
+                shopChoice = "3";
+                return;
             }
-            
+            if((this.health += 20) > 100) {
+                this.health = 100;
+                this.money -=10;
+            }
+            else {
+                //does 2 +20's for some reason, don't know why, reducing to 10 to acheive outcome of healing of 20
+                this.health += 10;
+                this.money -= 10;
+            }
+        },
+        increaseAttack: function(){
+            if(this.money <20){
+                if(this.money <10){
+                    window.alert("You don't have enough money for this. Continuing to the next match.\n\nGood luck, Pit Dog");
+                    shopChoice = "3";
+                    return;
+                }
+                window.alert("You don't have enough money for this.\n\nGood luck, Pit Dog");
+                return;
+
+            }
+            this.attack +=3;
+            this.money -= 20;
         }
-        window.alert("Welcome to the robot gladitorial arena!\n\nRound " + (i+1) + " of " + attackerNames.length + "!\n\nFight!");
-        var playerStats = fight(attackerNames[i], attackerHealth, attackerAttack, playerAlive, playerAttack, playerHealth, playerName, playerMoney);
-        playerHealth = playerStats[0];
-        attackerAttack = playerStats[1];
-        playerMoney = playerStats[2];
-        playerAlive = playerStats[3];
+    };
+    var names = ["Roberto", "Bender", "C3-P0"];
+    var attackers = {
+        name: "",
+        health: 50,
+        attack: 12
+    };
+    var shopChoice;
+    var playerStats;
+
+    while(repeat){
+        player.reset();
+        shopChoice = 1;
+        //commences fight
+        for(var i=0; i<names.length; i++){
+            if(!player.alive) break;
+            shopChoice = 1;
+            if(i>0){
+                while(shopChoice != "3"){
+
+                    shopChoice = window.prompt("Welcome to the shop, You can 1.Refill: 10 gold, 2.Upgrade: 20 gold, 3.Leave\n\nYour Health: "+ player.health +
+                     "\nYour Attack: " + player.attack +
+                     "\nYour gold: "+ player.money +"\n\nEnter 1, 2, or 3:");
+
+                    while(shopChoice != 1 && shopChoice != 2 && shopChoice != 3) shopChoice = window.prompt("ERROR: Invalid selection, your selection, " + shopChoice + ". Acceptable selections are 1, 2, 3.\n\nWelcome to the shop, You can 1.Refill: 10 gold, 2.Upgrade: 20 gold, 3.Leave\n\nYour gold: "+ player.money +"\n\nEnter 1, 2, or 3:");
+                    switch(shopChoice){
+                        case "1":   player.refillHealth();
+                                    break;
+                        
+                        case "2":   player.increaseAttack();  
+                                    
+                        
+                        default:     break;
+                    }
+                }
+                
+            }
+            attackers.name = names[i];
+            window.alert("Welcome to the robot gladitorial arena!\n\nRound " + (i+1) + " of " + names.length + "!\n\nFight!");
+            attackers.health = randomNumber(40, 70);
+            attackers.attack = randomNumber(10, 15);
+            playerStats = fight(attackers, player);
+            player = playerStats;
+        }
+        if(i == names.length && player.alive == true) window.alert("Congratulations, Grand Champion of The Arena!!!");
+        repeat = window.confirm("Play again?!");
     }
-    if(i == attackerNames.length && playerAlive == true) window.alert("Congratulations, Grand Champion of The Arena!!!");
-    var repeat = window.confirm("Play again?!");
-    if(repeat) startGame();
 }
 
-function fight(attacker, attackerHealth, attackerAttack, playerAlive, playerAttack, playerHealth, playerName, playerMoney){
+function fight(attackers, player){
     var healths;
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    while(attackerHealth > 0 && playerHealth > 0){
+    //Subtract the value of `player.attack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    while(attackers.health > 0 && player.health > 0){
         //defaults to let them pick their answer
         var skip = "a";
         //controls error message prompt
@@ -85,62 +117,62 @@ function fight(attacker, attackerHealth, attackerAttack, playerAlive, playerAtta
         skip = skip.toLowerCase();
         if(skip === "s" || skip === "skip"){
             var confirm = window.confirm("Are you sure you want to skip? There will be a 10 gold penalty.");
-            if(playerMoney <= 0) window.alert("You have no money to left!\n\nBack to the fight with you, Pit Dog!")
+            if(player.money <= 0) window.alert("You have no money to left!\n\nBack to the fight with you, Pit Dog!")
             else if(confirm){
                         window.alert("You have chosen to skip this fight!");
-                        playerMoney -= 10;
+                        player.money -= 10;
                         window.alert("You lost 10 gold!");
                         continue;
             }
             
-            healths = attack(attacker, attackerAttack, attackerHealth, playerHealth, playerName, playerAttack);
-            attackerHealth = healths[0];
-            playerHealth = healths[1];
+            healths = attack(attackers, player);
+            attackers = healths[0];
+            player = healths[1];
         }
         else{
-            healths = attack(attacker, attackerAttack, attackerHealth, playerHealth, playerName, playerAttack);
-            attackerHealth = healths[0];
-            playerHealth = healths[1];
+            healths = attack(attackers, player);
+            attackers = healths[0];
+            player = healths[1];
         }
     }
-    if(playerHealth > 0){
-        window.alert(playerName + " Wins!");
-        playerMoney += 20;
+    if(player.health > 0){
+        window.alert(player.name + " Wins!");
+        player.money += 20;
         window.alert("You gained 20 gold!");
-        return [playerHealth, playerAttack, playerMoney, playerAlive];
+        return player;
     } else{
         window.alert("You Lose!");
-        playerAlive = false;
-        return [playerHealth, playerAttack, playerMoney, playerAlive];
+        player.alive = false;
+        return player;
     }
 }
 
 //employing DRY
-function attack(attacker, attackerAttack, attackerHealth, playerHealth, playerName, playerAttack){
-    if((attackerHealth-playerAttack) < 0) attackerHealth = 0;
+function attack(attackers, player){
+    if((attackers.health-player.attack) < 0) attackers.health = 0;
     else {
-        var damage = randomNumber(playerAttack-3, playerAttack);
-        attackerHealth -= Math.max(0, damage);
+        var damage = randomNumber(player.attack-3, player.attack);
+        attackers.health -= Math.max(0, damage);
     }
     // Log a resulting message to the console so we know that it worked.
-    window.alert(attacker + "'s Health: " + attackerHealth + ", decreased by " + playerAttack + " from " + (attackerHealth+playerAttack));
-    if(attackerHealth === 0){
-        window.alert(attacker + " has died!");
+    window.alert(attackers.name + "'s Health: " + attackers.health + ", decreased by " + player.attack + " from " + (attackers.health+player.attack));
+    if(attackers.health === 0){
+        window.alert(attackers.name + " has died!");
         //kills while loop
-        return [attackerHealth, playerHealth];
+        return [attackers, player];
     }
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    if((playerHealth-attackerAttack) < 0) playerHealth = 0;
+    // Subtract the value of `enemyAttack` from the value of `player.health` and use that result to update the value in the `player.health` variable.
+    if((player.health-attackers.attack) < 0) player.health = 0;
     else {
-        var damage = randomNumber(attackerAttack-3, attackerAttack);
-        playerHealth -= Math.max(0, damage);
+        var damage = randomNumber(attackers.attack-3, attackers.attack);
+        player.health -= Math.max(0, damage);
     }
     // Log a resulting message to the console so we know that it worked.
-    window.alert(playerName + "'s Health: " + playerHealth + ", decreased by " + attackerAttack + " from " + (playerHealth+attackerAttack));
-    if(playerHealth === 0){
-        window.alert(playerName + " has died!");
+    window.alert(player.name + "'s Health: " + player.health + ", decreased by " + attackers.attack + " from " + (player.health+attackers.attack));
+    if(player.health === 0){
+        window.alert(player.name + " has died!");
    }
-   return [attackerHealth, playerHealth];
+   return [attackers, player];
 }
 
 function randomNumber(min, max){
